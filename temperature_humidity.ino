0a61444478fe2ff1a2817b69883a25e916232dbc
G. Lee Clark, II <gclark2@gmail.com>
2016-02-23 22:34:22 -0500

    Add Humidity Graphing

--- temperature_humidity.ino
+++ temperature_humidity.ino
@@ -28,6 +28,24 @@ void drawTempGraph() {
   server.send ( 200, "image/svg+xml", out);
 }
 
+void drawHumidityGraph() {
+ String out = "";
+  char humidity[100];
+  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
+  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
+  out += "<g stroke=\"blue\">\n";
+  int y = hdc.readHumidity();
+  for (int x = 10; x < 390; x+= 10) {
+    int y2 = rand() % 100;
+    sprintf(humidity, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
+    out += humidity;
+    y = y2;
+  }
+  out += "</g>\n</svg>\n";
+
+  server.send ( 200, "image/svg+xml", out);
+}
+
 void setupMonitor() {
 
   Serial.println("HDC100x test");
