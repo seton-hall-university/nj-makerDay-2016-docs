f95463225abdf37208c9818d1806467fafced65d
G. Lee Clark, II <gclark2@gmail.com>
2016-02-23 22:46:02 -0500

    Removes commented lines

--- njmakerday_20160318.ino
+++ njmakerday_20160318.ino
@@ -115,24 +115,6 @@ void drawUptimeGraph() {
   server.send ( 200, "image/svg+xml", out);
 }
 
-//void drawTempGraph() {
-// String out = "";
-//  char temp[100];
-//  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-//  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-//  out += "<g stroke=\"blue\">\n";
-//  int y = hdc.readTemperature();
-//  for (int x = 10; x < 390; x+= 10) {
-//    int y2 = rand() % 100;
-//    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-//    out += temp;
-//    y = y2;
-//  }
-//  out += "</g>\n</svg>\n";
-//
-//  server.send ( 200, "image/svg+xml", out);
-//}
-
 void setup ( void ) {
 	pinMode ( led, OUTPUT );
 	digitalWrite ( led, 0 );
@@ -163,14 +145,6 @@ void setup ( void ) {
 		Serial.println ( "MDNS responder started" );
 	}
 
-//  // Setup the temp & humidity monitor;
-//  Serial.println("HDC100x test");
-//  // Set SDA and SDL ports
-//  Wire.begin(hdc_sda, hdc_scl);
-//  if (!hdc.begin()) {
-//    Serial.println("Couldn't find sensor!");
-//    while (1);
-//  }
   setupMonitor();
   
 	server.on ( "/", handleRoot );
@@ -187,15 +161,4 @@ void setup ( void ) {
 void loop ( void ) {
 	server.handleClient();
   readMonitors();
-//  int currentTempC = hdc.readTemperature();
-//  int currentTempF = hdc.readTemperature() * 9/5 + 32;
-//  
-//  Serial.print("Temp: "); 
-//  Serial.print(currentTempC);
-//  Serial.print("C / ");
-//  Serial.print(currentTempF);
-//  Serial.print("F"); 
-//  Serial.print("\tHum: "); 
-//  Serial.println(hdc.readHumidity());
-//  delay(1000);
 }
