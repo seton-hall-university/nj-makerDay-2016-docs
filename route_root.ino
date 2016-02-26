dc31e8508d410d5153f2174d64baa6f0ba337ad6
G. L. Clark, II <clarkgar@shu.edu>
2016-02-26 15:20:38 -0500

    Updates Root Route
    Updates route to display a simplified page with the current sensor readings.

--- route_root.ino
+++ route_root.ino
@@ -2,30 +2,41 @@
 #include "Arduino.h"
 
 void handleRoot() {
- digitalWrite ( ledWiFi, 1 );
-  char temp[600];
+ digitalWrite ( ledWiFi, 0 );
+  char temp[1024];
   int sec = millis() / 1000;
   int min = sec / 60;
   int hr = min / 60;
 
-  snprintf ( temp, 600,
+  // Convert the floats to strings
+  char tc[6];
+  char tf[6];
+  char hm[6];
+  dtostrf(getTempCelsius(), 3, 2, tc);
+  dtostrf(getTempFahrenheit(), 3, 2, tf);
+  dtostrf(getHumidityRH(), 3, 2, hm);
+
+  snprintf ( temp, 1024,
 
 "<html>\
   <head>\
     <meta http-equiv='refresh' content='5'/>\
-    <title>ESP8266 Demo</title>\
+    <title>ESP8266 Temp/Humidity Sensors</title>\
     <style>\
       body { background-color: #cccccc; font-family: Arial, Helvetica, Sans-Serif; Color: #000088; }\
     </style>\
   </head>\
   <body>\
     <h1>Hello from ESP8266!</h1>\
-    <p>Uptime: %02d:%02d:%02d</p>\
+    <p><strong>Uptime</strong>: %02d:%02d:%02d</p>\
+    <p><strong>Celsius</strong>: %s C</p>\
+    <p><strong>Fahrenheit</strong>: %s F</p>\
+    <p><strong>Humidity</strong>: %s RH</p>\
   </body>\
 </html>",
 
-    hr, min % 60, sec % 60
+    hr, min % 60, sec % 60, tc, tf, hm
   );
   server.send ( 200, "text/html", temp );
-//  digitalWrite ( ledWiFi, 0 );
+  digitalWrite ( ledWiFi, 1 );
 }
