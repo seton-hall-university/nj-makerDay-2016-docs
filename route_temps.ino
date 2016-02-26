6bae5c7937c8dd7c48823859b8a9a6bb5bde8295
G. L. Clark, II <clarkgar@shu.edu>
2016-02-26 13:02:37 -0500

    Updates Temperature Router
    Updates main route to return an object instead of an array.
    
    Adds routes for each unit of measure.

--- route_temps.ino
+++ route_temps.ino
@@ -2,12 +2,42 @@
 #include "Arduino.h"
 
 void handleTemperature() {
+  float currentTemp = hdc.readTemperature();
   String out = "";
-  out += "[{\"currentTempC\":";
-  out += hdc.readTemperature();
-  out += "},{\"currentTempF\":";
-  out += hdc.readTemperature() * 9/5 + 32;
-  out += "}]";
+  out += "{\"temperature\":{\"celsius\":";
+  out += currentTemp;
+  out += ",\"fahrenheit\":";
+  out += currentTemp * 9/5 + 32;
+  out += ",\"kelvin\":";
+  out += currentTemp + 273.15;
+  out += "}}";
+  server.send ( 200, "application/json", out );
+}
+
+void handleTemperatureC() {
+  float currentTemp = hdc.readTemperature();
+  String out = "";
+  out += "{\"temperature\":{\"celsius\":";
+  out += currentTemp;
+  out += "}}";
+  server.send ( 200, "application/json", out );
+}
+
+void handleTemperatureF() {
+  float currentTemp = hdc.readTemperature();
+  String out = "";
+  out += "{\"temperature\":{\"fahrenheit\":";
+  out += currentTemp * 9/5 + 32;
+  out += "}}";
+  server.send ( 200, "application/json", out );
+}
+
+void handleTemperatureK() {
+  float currentTemp = hdc.readTemperature();
+  String out = "";
+  out += "{\"temperature\":{\"kelvin\":";
+  out += currentTemp + 273.15;
+  out += "}}";
   server.send ( 200, "application/json", out );
 }
 
