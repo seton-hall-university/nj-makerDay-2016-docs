3010438de90c6505a9b64df909719c656bb02d68
G. L. Clark, II <clarkgar@shu.edu>
2016-02-26 15:21:00 -0500

    Updates references.

--- zz_references.ino
+++ zz_references.ino
@@ -1,8 +1,11 @@
 /* Credits and References (that I can remember...)
+ * Report the temperature with ESP8266 to MQTT => https://home-assistant.io/blog/2015/10/11/measure-temperature-with-esp8266-and-report-to-mqtt/
  * Home Automation in the Cloud with the ESP8266 & Adafruit IO => https://learn.adafruit.com/home-automation-in-the-cloud-with-the-esp8266-and-adafruit-io/programming-the-modules
  *  Github => https://github.com/openhomeautomation/adafruit-io-esp8266/blob/master/esp8266_sensor_module/esp8266_sensor_module.ino
  * Many, many Google searches.
  * jQuery => http://jquery.com
+ * Adafruit HDC1008 Temperature & Humidity Sensor Breakout Board => https://www.adafruit.com/products/2635
  * Humidity Information => https://en.wikipedia.org/wiki/Humidity
  * Temperature Information => https://en.wikipedia.org/wiki/Category:Units_of_temperature
+ * Converting floats to decimals => http://www.hobbytronics.co.uk/arduino-float-vars
  */
