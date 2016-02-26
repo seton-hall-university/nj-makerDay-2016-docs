8ba1c662e570db66b28cc2f52549d7e050ec6137
G. L. Clark, II <clarkgar@shu.edu>
2016-02-26 12:57:47 -0500

    Updates reference file.

--- zz_references.ino
+++ zz_references.ino
@@ -2,5 +2,7 @@
  * Home Automation in the Cloud with the ESP8266 & Adafruit IO => https://learn.adafruit.com/home-automation-in-the-cloud-with-the-esp8266-and-adafruit-io/programming-the-modules
  *  Github => https://github.com/openhomeautomation/adafruit-io-esp8266/blob/master/esp8266_sensor_module/esp8266_sensor_module.ino
  * Many, many Google searches.
-
+ * jQuery => http://jquery.com
+ * Humidity Information => https://en.wikipedia.org/wiki/Humidity
+ * Temperature Information => https://en.wikipedia.org/wiki/Category:Units_of_temperature
  */
