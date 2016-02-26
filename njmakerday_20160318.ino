dd37e33a6cb0b8afe96b2c3aaa3fc8fa47093b01
G. L. Clark, II <clarkgar@shu.edu>
2016-02-26 14:02:08 -0500

    Removes MQTT

--- njmakerday_20160318.ino
+++ njmakerday_20160318.ino
@@ -5,8 +5,6 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 #include "Adafruit_HDC1000.h"
-#include <Adafruit_MQTT.h>
-#include <Adafruit_MQTT_Client.h>
 #include <Wire.h>
 
 Adafruit_HDC1000 hdc = Adafruit_HDC1000();
@@ -40,47 +38,17 @@ float currentHumidity = 0.000;
 // Functions
 void connectWiFi();
 char getWiFiAddress(void);
-void mqttConnect();
-void mqttPublish();
 void handleRoot();
 void handleNotFound();
 void handleTemperature();
 void handleHumidity();
+int getTempCelsius(void);
+int getTempFahrenheit(void);
+int getTempKelvin(void);
+int getHumidityRH(void);
 
-// Create an ESP8266 WiFiClient class to connect to the MQTT server.
+// Create an ESP8266 WiFiClient class.
 WiFiClient client;
-
-#define AIO_SERVER      "io.adafruit.com"
-#define AIO_SERVERPORT  1883
-#define AIO_USERNAME    "ProfeC"
-#define AIO_KEY         "d85b8f1095d5f17816b3178be9e1ed7ea4e199df"
-#define FEED_PATH_TEMPC AIO_USERNAME "/feeds/temperature-c";
-#define FEED_PATH_TEMPF AIO_USERNAME "/feeds/temperature-f";
-#define FEED_PATH_HUMIDITY AIO_USERNAME "/feeds/humidity";
-
-// Store the MQTT server, client ID, username, and password in flash memory.
-const char MQTT_SERVER[] PROGMEM    = AIO_SERVER;
-
-// Set a unique MQTT client ID using the AIO key + the date and time the sketch
-// was compiled (so this should be unique across multiple devices for a user,
-// alternatively you can manually set this to a GUID or other random value).
-const char MQTT_CLIENTID[] PROGMEM  = AIO_KEY __DATE__ __TIME__;
-const char MQTT_USERNAME[] PROGMEM  = AIO_USERNAME;
-const char MQTT_PASSWORD[] PROGMEM  = AIO_KEY;
-
-// Setup the MQTT client class by passing in the WiFi client and MQTT server and login details.
-Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, AIO_SERVERPORT, MQTT_CLIENTID, MQTT_USERNAME, MQTT_PASSWORD);
-
-/****************************** Feeds ***************************************/
-// Setup feeds for temperature & humidity
-const char TEMPERATURE_C_FEED[] PROGMEM = FEED_PATH_TEMPC;
-Adafruit_MQTT_Publish temperature_c = Adafruit_MQTT_Publish(&mqtt, TEMPERATURE_C_FEED);
-
-const char TEMPERATURE_F_FEED[] PROGMEM = FEED_PATH_TEMPF;
-Adafruit_MQTT_Publish temperature_f = Adafruit_MQTT_Publish(&mqtt, TEMPERATURE_F_FEED);
-
-const char HUMIDITY_FEED[] PROGMEM = FEED_PATH_HUMIDITY;
-Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, HUMIDITY_FEED);
 
 /*************************** Sketch Code ************************************/
 void setup ( void ) {
@@ -90,9 +58,6 @@ void setup ( void ) {
   
   // Connect to Wifi
   connectWiFi();
-
-  // connect to adafruit io
-//  mqttConnect();
   
   Serial.println("Setting up HDC100x...");
   Wire.begin(hdc_sda, hdc_scl);
@@ -133,9 +98,6 @@ void loop ( void ) {
   Serial.print("F"); 
   Serial.print("\tHum: "); 
   Serial.println(currentHumidity);
-
-  // Publish data
-//  mqttPublish();
 
 //  digitalWrite ( ledWiFi, 1 );
 //  delay(180000); // 3 minutes
